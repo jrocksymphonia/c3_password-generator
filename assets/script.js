3315db066d028b5d2c3a811f867cf5e30bc28a87
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-02-22 17:06:29 -0500

    defined password length as var passwordLength

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,9 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  window.prompt("Set character length of password. It can be 8-128 characters long");
+  var passwordLength = window.prompt("Set character length of password. It can be 8-128 characters long");
+
+  
 
 
 
